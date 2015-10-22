bc804c29c2a5106e7053e0026c6aa02418c4b9d4
guillaumervls <guillaumervls@server.fake>
2015-10-22 12:49:19 +0200

    refactor

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM quay.io/sizio/ghost-siz
-
-MAINTAINER Fred-b for Siz <frederic.boulet@gmail.com>
-
-ADD * /$GHOST_SOURCE/content/themes/ghost-wbkd-siz/
-
---
+++ config.js
@@ -0,0 +1,55 @@
+// # Ghost Configuration
+// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
+
+// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
+
+var path = require('path');
+var env = process.env;
+var isDev = ((env.NODE_ENV || 'development') === 'development');
+var conf = {};
+
+conf.server = {
+  host: '0.0.0.0',
+  port: '2368'
+};
+
+conf.paths = {
+  contentPath: env.GHOST_CONTENT
+}
+
+conf.url = env.GHOST_URL || (isDev ? 'http://localhost:8080' : 'http://siz.io')
+
+conf.mail = (env.MAIL_SERVICE || env.MAIL_USER || env.MAIL_PASSWORD) ? {
+  transport: 'SMTP',
+  options: {
+    service: env.MAIL_SERVICE || 'Gmail',
+    auth: {
+      user: env.MAIL_USER,
+      pass: env.MAIL_PASSWORD
+    }
+  }
+} : {};
+
+conf.mail.from = env.MAIL_FROM;
+
+conf.fileStorage = false;
+
+conf.database = env.DB_HOST ? {
+  client: 'mysql',
+  connection: {
+    host: env.DB_HOST,
+    user: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME
+  }
+} : {
+  client: 'sqlite3',
+  connection: {
+    filename: path.join(env.GHOST_CONTENT, '/data/ghost.db')
+  }
+};
+
+module.exports = {
+  production: conf,
+  development: conf
+};
