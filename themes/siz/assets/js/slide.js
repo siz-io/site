a4b6758990e6013cc944c46de1f9f70d96c6998b
Sofiane <theabstractdev@gmail.com>
2015-10-27 12:04:03 +0100

    animation menu

--- themes/siz/assets/js/slide.js
+++ themes/siz/assets/js/slide.js
@@ -1,22 +1,23 @@
-var bars = document.getElementById("bars"),
-side = document.getElementById("mobile-taglist-wrapper"),
-elHeight = side.offsetHeight,
-tmp = 0;
+$(function()
+{
+	var bars = $("#bars"),
+	side = $("#mobile-taglist-wrapper"),
+	elHeight = side.height(),
+	tmp = 0;
 
-bars.addEventListener('click', function(e) {
-	if (tmp == 0) {
-		tmp = 1;
-		console.log("up");
-		// side.classList.remove("animateSideBack");
-		// side.classList.add("animateSide");
-		side.style.marginTop = "0";
-	} else {
-		tmp = 0;
-		console.log("down");
-		// side.classList.remove("animateSide");
-		// side.classList.add("animateSideBack");
-		console.log(elHeight);
-		side.style.marginTop = -elHeight + "px";
-	}
+	bars.click(function(e) {
+		if (tmp == 0) {
+			tmp = 1;
+			side.animate({
+				marginTop: 0,
+			}, 300);
+		} else {
+			tmp = 0;
+			console.log(elHeight);
+			side.animate({
+				marginTop: -elHeight + "px",
+			}, 300);
+		}
 
+	});
 });
