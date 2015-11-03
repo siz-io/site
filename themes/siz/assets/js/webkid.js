a854066ae15afa0b6dacdbad4b6f54a9c1f9c0e8
Sofiane <theabstractdev@gmail.com>
2015-11-03 18:33:11 +0100

    debut fix thumbnails

--- themes/siz/assets/js/webkid.js
+++ themes/siz/assets/js/webkid.js
@@ -15,7 +15,8 @@ $(function() {
       var p = posts[i];
       var date = new Date(p.pubDate);
       var dateStr = date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
-      var $a = $('<a href="' + p.link + '"><div class="date">' + dateStr + '</div><div>' + '<img class="thumb" src="' + p.content.url +'"><span class="thumbSpan">' + p.title + '</span></div></a>');
+      var $a = $('<a href="' + p.link + '"><div class="date">' + dateStr + '</div><div><img src="' + p.content.url +'" class="thumb" /><span class="thumbSpan">' + p.title + '</span> ' + '</div></a>');
+      
       if(i == 4) {
         $a.addClass('last');
       }
