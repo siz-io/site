58f5ff30f70395bfca46d2ce5ad42ae68c2a0e60
Sofiane <theabstractdev@gmail.com>
2015-11-25 15:55:31 +0100

    added verification to display mgid ads or not

--- themes/siz/assets/js/index.js
+++ themes/siz/assets/js/index.js
@@ -38,6 +38,7 @@
         $(".post-content img").addClass('post-img');
         $(".post-content a").addClass('post-link');
         var $postImg = $(".post-img");
+        var inputHidden = document.getElementById("input-hidden");
         $postContent.fitVids();
 
         if (window.location.search == "?src=tum") {
@@ -47,12 +48,16 @@
         }
 
         if( /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-            $(".post-content .fluid-width-video-wrapper").after('<iframe class="mgid" src="/assets/html/ad-mgid-1-3.html"></iframe>');
-            $(".post-content .fb-video").after('<iframe class="mgid-ad" src="/assets/html/ad-mgid-1-3.html"></iframe>');
+            if(inputHidden && inputHidden.value === "mgid") {
+                $(".post-content .fluid-width-video-wrapper").after('<iframe class="mgid" src="/assets/html/ad-mgid-1-3.html"></iframe>');
+                $(".post-content .fb-video").after('<iframe class="mgid-ad" src="/assets/html/ad-mgid-1-3.html"></iframe>');
+            }
         } else {
-            $(".post-content .fluid-width-video-wrapper").after('<iframe class="mgid" src="/assets/html/ad-mgid-4-2.html"></iframe>');
-            $(".post-content .fb-video").after('<iframe class="mgid-ad" src="/assets/html/ad-mgid-4-2.html"></iframe>');
-
+            if(inputHidden && inputHidden.value === "mgid") {
+                $(".post-content .fluid-width-video-wrapper").after('<iframe class="mgid" src="/assets/html/ad-mgid-4-2.html"></iframe>');
+                $(".post-content .fb-video").after('<iframe class="mgid-ad" src="/assets/html/ad-mgid-4-2.html"></iframe>');
+            }
+            
             $(".content.clearfix").before("<div class='ad ad-banner-top'></div>");
             $(".content.clearfix").after("<div class='ad ad-banner-bottom'></div>");
 
