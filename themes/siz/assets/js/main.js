e410a686feddec0633271fd593e92459bb2d7143
Sofiane <theabstractdev@gmail.com>
2015-12-01 16:08:18 +0100

    added cookie consent on header + removed disableMgid variable (never used)

--- themes/siz/assets/js/main.js
+++ themes/siz/assets/js/main.js
@@ -81,7 +81,6 @@
     $('.post-content img').addClass('post-img')
     $('.post-content a').addClass('post-link')
     var $postImg = $('.post-img')
-    var disableMgid = $('#disable-mgid')
     $postContent.fitVids()
 
     var ghostAsset = function (path) {
@@ -89,7 +88,7 @@
     }
 
     if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-      $("#taglist li:nth-child(4)").nextAll().remove()
+      $('#taglist li:nth-child(4)').nextAll().remove()
       $('.post-content .fluid-width-video-wrapper').after('<iframe scrolling="no" class="ad-taboola-1-3" src="' + ghostAsset('html/ad-taboola.html') + '&mode=thumbnails-a&id=taboola-below-video-player-thumbnails&placement=' + encodeURIComponent('Below Video Player Thumbnails') + '"></iframe>')
       $('.post-content .fb-video').after('<iframe scrolling="no" class="ad-taboola-1-3" src="' + ghostAsset('html/ad-taboola.html') + '"></iframe>')
     } else {
