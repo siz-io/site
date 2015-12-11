a9e2cb9ab976ef5ca2cfbb5805b735e748451364
Sofiane <theabstractdev@gmail.com>
2015-12-11 17:12:03 +0100

    added border to criteo ads on posts

--- themes/siz/assets/js/main.js
+++ themes/siz/assets/js/main.js
@@ -38,6 +38,7 @@
     $('.ad-banner-middle-sidebar .adsbygoogle').css({'width': '302px', 'height': '252px'})
     $('.ad-banner-top .adsbygoogle').css({'width': '730', 'height': '92px'})
     $('.ad-banner-post .adsbygoogle').css({'width': '730', 'height': '92px'})
+    $('.ad-criteo-desktop-above-video .adsbygoogle').css({'width': '730', 'height': '92px'})
     if (!(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent))) {
       $('iframe#instagram-embed-0').css({'width': '660px', 'margin': 'auto'})
       var sidebarTop = $('.ad.ad-banner-middle-sidebar').offset().top - 170
